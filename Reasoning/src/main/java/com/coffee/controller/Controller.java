c52e646514bca18ddac2c2922e4633f6be20db18
saradrada <saradrada@hotmail.com>
2019-06-17 10:35:56 -0500

    Reasoning API REST structure with method's signatures

---
+++ Reasoning/src/main/java/com/coffee/controller/Controller.java
@@ -0,0 +1,49 @@
+package com.coffee.controller;
+
+import org.json.simple.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@CrossOrigin
+	@RequestMapping(value = "/validModel", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public boolean validModel(@RequestBody JSONObject data) throws Exception {
+
+		boolean isValid = true; 
+		System.out.println(data.get("data"));
+		return isValid;
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value = "/oneConfiguration", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public String oneConfiguration(@RequestBody JSONObject model, @RequestBody JSONObject configuration) throws Exception {
+
+		System.out.println("Model: " + model.get("data"));
+		System.out.println("Configuration: " + configuration.get("data"));
+		
+		String solution = "";
+		return solution;
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value = "/nConfigurations", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public String nConfigurations(@RequestBody int n, @RequestBody JSONObject configuration) throws Exception {
+
+		System.out.println("Number of configurations: " + n);
+		System.out.println("Configuration: " + configuration.get("data"));
+		
+		String solution = "";
+		return solution;
+	}
+	
+}
