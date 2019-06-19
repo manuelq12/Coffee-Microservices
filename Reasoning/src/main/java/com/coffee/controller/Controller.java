4a1beb234ee0756fc67f88345160a33a259bccc4
Joan David <Joancolina819>
2019-06-19 09:44:46 -0500

    RESTAPI path has been changed

--- Reasoning/src/main/java/com/coffee/controller/Controller.java
+++ Reasoning/src/main/java/com/coffee/controller/Controller.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
 	@CrossOrigin
-	@RequestMapping(value = "/validModel", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/reasoning/validModel", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public boolean validModel(@RequestBody JSONObject data) throws Exception {
 
@@ -23,26 +23,26 @@ public class Controller {
 	}
 	
 	@CrossOrigin
-	@RequestMapping(value = "/oneConfiguration", method = RequestMethod.POST, produces = "application/json")
+	@RequestMapping(value = "/reasoning/oneConfiguration", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
 	public String oneConfiguration(@RequestBody JSONObject model, @RequestBody JSONObject configuration) throws Exception {
 
 		System.out.println("Model: " + model.get("data"));
 		System.out.println("Configuration: " + configuration.get("data"));
 		
-		String solution = "";
+		String solution = "One configuration";
 		return solution;
 	}
 	
 	@CrossOrigin
-	@RequestMapping(value = "/nConfigurations", method = RequestMethod.POST, produces = "application/json")
+	@RequestMapping(value = "/reasoning/nConfigurations", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
 	public String nConfigurations(@RequestBody int n, @RequestBody JSONObject configuration) throws Exception {
 
 		System.out.println("Number of configurations: " + n);
 		System.out.println("Configuration: " + configuration.get("data"));
 		
-		String solution = "";
+		String solution = "N configuration";
 		return solution;
 	}
 	
